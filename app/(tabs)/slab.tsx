90917899b4a1a5599e7e09a1be6ae0451cbc1682
markdauz <mark.legal.dauz@gmail.com>
2026-05-01 07:58:59 +0800

    layout setup

---
+++ app/(tabs)/slab.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Slab() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: '#000' }}>Slab Screen</Text>
+    </View>
+  );
+}
